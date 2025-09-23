3b87003ac5e4a1521cf7e02ed196c7d754af3bd8
muhfahmm <148422221+muhfahmm@users.noreply.github.com>
2025-09-23 19:56:54 +0700

    vote

---
+++ pages/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="wrapper">
+            <div class="kandidat">
+                <div class="ketua">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <div class="data-user">
+                        <h3>Nama Kandidat Ketua</h3>
+                        <p>Kelas: X-1</p>
+                        <p>Calon Ketua</p>
+                    </div>
+                </div>
+                <div class="wakil">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <div class="data-user">
+                        <h3>Nama Kandidat Ketua</h3>
+                        <p>Kelas: X-1</p>
+                        <p>Calon Ketua</p>
+                    </div>
+                </div>
+                <div class="form-user">
+                    <form action="" method="post">
+                        <label for="nama">Nama Pemilih</label>
+                        <input type="text" name="pemilih">
+                        <label for="kelas">Kelas Pemilih</label>
+                        <input type="text" name="kelas">
+                        <button type="submit" name="kirim">Kirim Vote</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
