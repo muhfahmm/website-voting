5c88f0b6e120dc71b208fafdbc7975dd0c1b7200
muhfahmm <148422221+muhfahmm@users.noreply.github.com>
2025-09-30 14:20:03 +0700

    vote

--- admin/hasil-vote/result.php
+++ admin/hasil-vote/result.php
@@ -144,7 +144,7 @@ $totalVotes = $totalRow['total'];
             <li><a href="result.php" class="active">📋 Hasil</a></li>
             <li><a href="../kandidat/tambah.php">➕ Tambah Kandidat</a></li>
             <li><a href="../kandidat/daftar.php">📋 Daftar Kandidat</a></li>
-            <li><a href="../kandidat/voter.php">📋 voter</a></li>
+            <li><a href="../kandidat/voter.php">👥 Daftar Voter</a></li>
             <li><a href="../auth/logout.php">🚪 Logout</a></li>
         </ul>
     </div>
